467adf8f602e71fd56932898fe65c23722cc2202
ccolombo71 <76749684+ccolombo71@users.noreply.github.com>
2024-10-25 09:08:37 +0200

    Update pronostici.py

--- pronostici.py
+++ pronostici.py
@@ -1,25 +1,13 @@
 import pandas as pd
-import sklearn
-from sklearn.tree import DecisionTreeClassifier
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
-import scikitplot as skplt
 import numpy as np
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import streamlit as st
-import seaborn as sns
-from sklearn.metrics import confusion_matrix
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
 
 st.set_page_config(page_title="Pronostici con il Machine Learning")
 st.title('Pronostici partite SERIE B con il Machine Learning')
 st.header('Pronostici partite per la stagione in corso')
 st.write(
     "Benvenuto all'applicazione Pronostici partite con il Machine Learning. "
-    "Questa applicazione utilizza modelli di Machine Learning per predire gli esiti delle partite di calcio "
+    "Questa applicazione utilizza modelli di Machine Learning per predire gli esiti delle partite di calcio. "
     "Scorri verso il basso per vedere le previsioni delle prossime partite e la classifica prevista."
 )
 np.random.seed(2)
@@ -28,13 +16,11 @@ def converti_data(data_str):
     try:
         return pd.to_datetime(data_str, format='%d/%m/%Y')
     except ValueError:
-        
         try:
             return pd.to_datetime(data_str, format='%d/%m/%y')
         except ValueError:
             print(f"Formato data non riconosciuto per: {data_str}")
             return None 
-
 
 # Carica i dati da ciascun URL in un DataFrame
 def load_dataframes():
@@ -42,16 +28,21 @@ def load_dataframes():
     for year in range(5, 24):
         url = f'https://www.football-data.co.uk/mmz4281/{year:02d}{year + 1:02d}/I2.csv'
         df_name = f'df{year:02d}'
-        globals()[df_name] = pd.read_csv(url)
-        dataframes[df_name] = globals()[df_name]
+        
+        # Carica il CSV specificando l'encoding e gestendo gli errori
+        try:
+            globals()[df_name] = pd.read_csv(url, encoding='ISO-8859-1')  # Specifica l'encoding qui
+            dataframes[df_name] = globals()[df_name]
+        except UnicodeDecodeError as e:
+            print(f"Errore di decoding per {url}: {e}")
+    
+    return dataframes
 
+# Carica tutti i DataFrame
 dataframes = load_dataframes()
 
-
-dfs = [df23, df22, df21, df20, df19, df18, df17, df16, df15, df14, df13, df12, df11, df10, df09, df08, df07, df06, df05]  # Aggiungi tutti i tuoi DataFrame qui
-
-
-
+# Specifica i DataFrame da utilizzare successivamente
+dfs = [dataframes.get(f'df{year:02d}', pd.DataFrame()) for year in range(23, 4, -1)]  # Modifica qui per usare i DataFrame caricati
 
 
 
